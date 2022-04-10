52622d7aad4eeae1fb8c0650af3fdf894fd9aa2a
Suresh Kumar <suresh.kumar@finastra.com>
2022-04-10 22:47:11 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# erb-fluentui-example
+
+**See the last commit for the required changes for fluentui integration**
+
+## Install
+
+```bash
+git clone --depth 1 --branch main https://github.com/yarli4u/erb-fluentui-example.git your-project-name
+cd your-project-name
+npm install
+```
---
+++ src/renderer/App.tsx
@@ -0,0 +1,60 @@
+import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
+import { Label } from '@fluentui/react/lib/Label';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const FluentUI = () => {
+  return (
+    <div>
+      <Label disabled>ERB + FluentUI = ❤</Label>
+    </div>
+  );
+};
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <FluentUI />
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
